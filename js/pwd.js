c49d13b451b157a469a9ce65da0c2fa2c97cb7f8
Michelle <yinm@usc.edu>
2017-01-09 15:47:51 -0800

    updated js file

---
+++ js/pwd.js
@@ -0,0 +1,6 @@
+	$('#submit').on('click', function() {
+    var input = document.getElementById("pwd").value;
+    if (input == "dtweekly4717!") {
+    	window.location.replace("cubes.html");
+    }
+	});
