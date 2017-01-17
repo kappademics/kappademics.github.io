94a9064513d13cc5eb6c7247f67b56cda23132fa
Michelle <yinm@usc.edu>
2017-01-17 15:56:54 -0800

    updated bitwise shifting and sheets

--- js/pwd.js
+++ js/pwd.js
@@ -1,6 +1,7 @@
 	$('#submit').on('click', function() {
     var input = document.getElementById("pwd").value;
-    if (input == "dtweekly4717!") {
+    var bitwise = input << 2;
+    if (bitwise == 18868) {
     	window.location.replace("cubes.html");
     }
 	});
